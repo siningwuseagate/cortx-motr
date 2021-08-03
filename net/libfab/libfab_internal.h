87de1231fb8af23bf84e95c02c04b2aa3cec01e4
Jaydeep Mohite <jaydeep.mohite@seagate.com>
2021-08-03 00:08:18 -0600

    Updated verbs segment config as 128k x 8
    Signed-off-by: Jaydeep Mohite <jaydeep.mohite@seagate.com>

--- net/libfab/libfab_internal.h
+++ net/libfab/libfab_internal.h
@@ -64,9 +64,9 @@ enum m0_fab__libfab_params {
 	/** Key used for memory registration. */
 	FAB_MR_KEY                     = 0xABCD,
 	/** Max number of IOV in read/write command for Verbs */
-	FAB_VERBS_IOV_MAX              = 16, //1,
+	FAB_VERBS_IOV_MAX              = 8, //1,
 	/** Max segment size for bulk buffers for Verbs */
-	FAB_VERBS_MAX_BULK_SEG_SIZE    = 16348, //1048576,
+	FAB_VERBS_MAX_BULK_SEG_SIZE    = 131072, //16348, //1048576,
 	/** Max number of active work requests for Verbs */
 	FAB_VERBS_MAX_QUEUE_SIZE       = 224,
 
